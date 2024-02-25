160ffffc7b7441202b4500fa296094faddfa2cc5
Dawson Childers <149831275+dawsonchilders@users.noreply.github.com>
2024-02-25 10:04:32 -0800

    adds starter model

---
+++ models/starter.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const starterSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  creationDate: {
+    type: Date,
+    default: Date.now,
+  },
+  notes: {
+    type: String,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true,
+});
+
+
+module.exports = mongoose.model('Starter', starterSchema);
